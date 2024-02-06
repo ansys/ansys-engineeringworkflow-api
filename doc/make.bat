6dc70a0169ba7eb402364dbbc23ff5e5f068645f
Camille <78221213+clatapie@users.noreply.github.com>
2024-02-06 14:20:45 +0100

    maint: updating cicd + using sphinx autoapi doc

--- doc/make.bat
+++ doc/make.bat
@@ -11,6 +11,16 @@ set SOURCEDIR=source
 set BUILDDIR=_build
 
 if "%1" == "" goto help
+if "%1" == "clean" goto clean
+
+REM TODO: these lines of code should be removed once the feature branch is merged
+for /f %%i in ('pip freeze ^| findstr /c:"sphinx-autoapi @ git+https://github.com/ansys/sphinx-autoapi"') do set is_custom_sphinx_autoapi_installed=%%i
+if NOT "%is_custom_sphinx_autoapi_installed%" == "sphinx-autoapi" (
+	pip uninstall --yes sphinx-autoapi
+	pip install "sphinx-autoapi @ git+https://github.com/ansys/sphinx-autoapi@feat/single-page-stable")
+REM TODO: these lines of code should be removed once the feature branch is merged
+
+if "%1" == "linkcheck" goto linkcheck
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -26,6 +36,15 @@ if errorlevel 9009 (
 )
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:clean
+rmdir /s /q %BUILDDIR% > /NUL 2>&1
+goto end
+
+:linkcheck
+%SPHINXBUILD% -b %1 %SPHINXOPTS% %SOURCEDIR% %LINKCHECKDIR%
+echo "Check finished. Report is in %LINKCHECKDIR%."
 goto end
 
 :help
