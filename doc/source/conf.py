db8f6b39b44731cdf7cbf775dd3e12340261525c
Camille <78221213+clatapie@users.noreply.github.com>
2024-02-07 15:50:28 +0100

    fix: pre-commit check

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -4,8 +4,7 @@ from datetime import datetime
 import os
 from pathlib import Path
 
-from ansys_sphinx_theme import (
-    # ansys_favicon,
+from ansys_sphinx_theme import (  # ansys_favicon,
     get_autoapi_templates_dir_relative_path,
     get_version_match,
     pyansys_logo_black,
