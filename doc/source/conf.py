2811aea3167b62a987db2270a5aec57c2aab0b56
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-02-15 10:01:17 +0100

    revert

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ html_theme_options = {
     "github_url": "https://github.com/ansys/ansys-engineeringworkflow-api",
     "show_prev_next": False,
     "show_breadcrumbs": True,
-    # "collapse_navigation": True,
+    "collapse_navigation": True,
     "use_edit_page_button": True,
     "additional_breadcrumbs": [
         ("PyAnsys", "https://docs.pyansys.com/"),
