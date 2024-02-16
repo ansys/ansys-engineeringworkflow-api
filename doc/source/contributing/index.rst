7982d123a825213d473a7e148b02d0fe86bc6db0
Kathy Pippert <kathy.pippert@ansys.com>
2024-02-16 14:12:05 -0500

    PY file edits

--- doc/source/contributing/index.rst
+++ doc/source/contributing/index.rst
@@ -107,8 +107,8 @@ of the template categories, you can click the link for opening a blank issue.
 
 To reach the PyAnsys support team, email `pyansys.core@ansys.com <pyansys.core@ansys.com>`_.
 
-Style and test
---------------
+Verify style and unit tests
+---------------------------
 
 If required, from the command line, you can call commands like `black`_, `isort`_, and `flake8`_.
 You can also call unit testing commands like `PyTest`_. However, running these commands does not
--- src/ansys/engineeringworkflow/api/datatypes.py
+++ src/ansys/engineeringworkflow/api/datatypes.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""API Datatype definitions."""
+"""API datatype definitions."""
 from __future__ import annotations
 
 from dataclasses import dataclass
@@ -31,55 +31,60 @@ from ansys.tools.variableinterop import IVariableValue
 
 @dataclass(frozen=True)
 class WorkflowEngineInfo:
-    """Information about a workflow engine as collected by get_server_info call."""
+    """Stores workflow engine information collected by the ``get_server_info`` call."""
 
     # TODO: this style documentation does not appear to be working?
     release_year: int
-    """The year portion of the release, such as 2022."""
+    """Year portion of the release, such as ``2022``."""
     release_id: int
-    """The id portion of the release, such as 2 for 2022 R2."""
+    """ID portion of the release, such as ``2`` for 2022 R2."""
     build: int
-    """The build number."""
+    """Build number."""
     is_release_build: bool
     """
-    True for production releases.
+    Whether the release is a production release.
 
-    False for development, alpha, beta, and other releases
+    The value is ``False`` for development, alpha, beta, and other releases.
     """
     build_type: str
     """
-    The build type.
+    Build type.
 
-    Must be blank for production releases. May include arbitrary information like the branch a
-    development build was built from.
+    This attribute must be blank for a production release. For other release types, it may include
+    arbitrary information like the branch a development build was built from.
     """
     version_as_string: str
     """
-    The version of the workflow engine encoded for human consumption.
+    Version of the workflow engine encoded for human consumption.
 
-    For example:
-    2022r1 build 333 ALPHA
+    For example, ```2022r1 build 333 ALPHA```.
     """
     server_type: str
     """
-    What server type is responding to this request.
+    Server type that is responding to the request.
 
-    Will be a string similar to 'optiSLang' or 'ModelCenter'
+    The value is a string similar to ``'ModelCenter'`` or ``'optiSLang'``.
     """
     install_location: Optional[str]
     """
-    If the client is on the same box as the workflow engine, it may optionally provide the
-    installation folder.
+    Installation directory.
 
-    Typically server based products do not provide this field for security reasons.
+    If the client is on the same box as the workflow engine, this attribute may optionally provide
+    the installation folder.
+
+    Typically server-based products do not provide this field for security reasons.
     """
     base_url: Optional[str]
-    """If this is a server based product ready to receive incoming connections from remote clients,
-    this field may be provided that gives the base URL for clients to connect to."""
+    """
+    Base URL for clients to connect to.
+
+    If this is a server-based product ready to receive incoming connections from remote clients,
+    this attribute may optionally provide the base URL for clients to connect to.
+    """
 
 
 class WorkflowInstanceState(Enum):
-    """The state that a workflow instance can be in."""
+    """Provides an enum with the states that a workflow instance can be in."""
 
     UNKNOWN = 0
     INVALID = 1
@@ -92,11 +97,12 @@ class WorkflowInstanceState(Enum):
 @dataclass(frozen=True)
 class Property:
     """
-    A configurable setting on some component or algorithm in the workflow.
+    Provides a configurable setting on some component or algorithm in the workflow.
 
-    Cannot be linked to other variables or properties. Unless a property is explicitly, documented
-    as supporting it, these values should not be changed while the workflow is running. Examples
-    that may support modification would be convergence criteria for an optimization algorithm.
+    This class cannot be linked to other variables or properties. Unless a property is explicitly
+    documented as supporting it, these values should not be changed while the workflow is running.
+    Examples that may support modification would be convergence criteria for an optimization
+    algorithm.
     """
 
     parent_element_id: str
